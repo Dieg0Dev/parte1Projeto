5f57ce4d856cb4db9a1d1b4c52083a0dbf66b739
Diego Bruno <ediego2117@gmail.com>
2022-05-27 23:03:15 -0300

    Criação do objeto banco e definição dos itens iniciais do menu

--- main.cpp
+++ main.cpp
@@ -5,7 +5,28 @@
 
 int main(){
 
+    Banco banco;
+    int op;
 
+	do{
+		std::cout << std::endl;
+		std::cout << "Selecione uma operação:" << std::endl;
+		std::cout << "1. Criar Conta" << std::endl;
+		std::cout << "6. Sair" << std::endl;
+		std::cout << std::endl;
+		std::cin >> op;
+		system("clear||cls");
+
+		switch(op){
+			case 1:
+				banco.cadastrarConta(); break;
+			case 6:
+				break;
+			default:
+				std::cout << "Opção inválida, digite novamente." << std::endl;
+		}
+
+	} while (op != 6);
 
     return 0;
 }
