ae04a8b46ad5593a2a7fa9ede38e516734eae7fe
Diego Bruno <ediego2117@gmail.com>
2022-05-27 23:19:23 -0300

    Adicionando texto no menu sobre credito e debito

--- main.cpp
+++ main.cpp
@@ -13,6 +13,8 @@ int main(){
 		std::cout << "Selecione uma operação:" << std::endl;
 		std::cout << "1. Criar Conta" << std::endl;
         std::cout << "2. Consultar Saldo" << std::endl;
+        std::cout << "3. Funcao Credito" << std::endl;
+		std::cout << "4. Funcao Debito" << std::endl;
 		std::cout << "6. Sair" << std::endl;
 		std::cout << std::endl;
 		std::cin >> op;
@@ -23,6 +25,10 @@ int main(){
 				banco.cadastrarConta(); break;
             case 2:
                 banco.consultarSaldo(); break;
+            case 3:
+                banco.funcaoCredito(); break;
+            case 4:
+                banco.funcaoDebito(); break;
 			case 6:
 				break;
 			default:
